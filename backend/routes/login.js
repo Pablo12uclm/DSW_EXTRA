b6355b5ead082df2200728b55bebdc41fed16a60
victorrg99 <44617510+victorrg99@users.noreply.github.com>
2024-06-28 10:40:33 +0200

    Delete backend directory

--- backend/routes/login.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Ruta para el inicio de sesión
-router.post('/', authController.login);
-
-module.exports = router;
